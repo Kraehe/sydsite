96398bdea0589a9b3e7d2a2d2963345adb1a2307
Gia <gigi.giggles01@gmail.com>
2024-08-06 04:51:43 -0400

    index prototype
    added index prototype

---
+++ script.js
@@ -0,0 +1,22 @@
+let anchor = document.getElementById("anchor");
+let screen = window.innerHeight 
+
+ document.addEventListener("DOMContentLoaded", function(event) { 
+ let anchor = document.getElementById("anchor");
+ let screen = window.innerHeight / 8
+
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+   if (document.body.scrollTop > screen || document.documentElement.scrollTop > screen) {
+     anchor.style.display = "block";
+   } else {
+     anchor.style.display = "none";
+   }
+ }
+  });
+
+ function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
+  } 
